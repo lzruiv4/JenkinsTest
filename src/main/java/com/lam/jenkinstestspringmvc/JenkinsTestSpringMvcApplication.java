a5a318e26211873d7e06f3bec5fd823b5cc061f5
lzruiv4 <lzruiv4@gmail.com>
2023-12-05 20:58:06 +0100

    Test

---
+++ src/main/java/com/lam/jenkinstestspringmvc/JenkinsTestSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package com.lam.jenkinstestspringmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsTestSpringMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsTestSpringMvcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lam/jenkinstestspringmvc/JenkinsTestSpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lam.jenkinstestspringmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsTestSpringMvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
